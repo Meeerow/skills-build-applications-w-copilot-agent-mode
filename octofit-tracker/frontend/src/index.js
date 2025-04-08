85498d81fb1ddbd5bdf37f36d5ddabffd1d52177
Meeerow <127819918+Meeerow@users.noreply.github.com>
2025-04-08 23:07:39 +0000

    Commit and push all recent changes in the workspace

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,11 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Use the new root API
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
 root.render(
   <React.StrictMode>
     <App />
